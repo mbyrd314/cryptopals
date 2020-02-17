e5b6d65fe776fb26cf4a95f91e6a6ed2c30a2e5d
Michael Byrd <42mbyrd@gmail.com>
2020-02-16 19:45:25 -0500

    Updated documentation

--- set1_challenge7.py
+++ set1_challenge7.py
@@ -4,6 +4,16 @@ from Crypto.Cipher import AES
 from base64 import b64encode, b64decode
 
 def decrypt_aes_ecb1(ciphertext, key):
+    """
+    Implementation of AES ECB mode decryption using the Python cryptography library
+
+    Args:
+        ciphertext (bytes): The ciphertext message to be decrypted
+        key (bytes): The AES secret key
+
+    Returns:
+        msg (bytes): The decrypted version of the ciphertext input
+    """
     backend = default_backend()
     cipher = Cipher(algorithms.AES(key), modes.ECB(), backend=backend)
     decryptor = cipher.decryptor()
@@ -11,13 +21,28 @@ def decrypt_aes_ecb1(ciphertext, key):
     return msg
 
 def decrypt_aes_ecb2(ciphertext, key):
+    """
+    Implementation of AES ECB mode decryption using the Python Crypto library
+
+    Args:
+        ciphertext (bytes): The ciphertext message to be decrypted
+        key (bytes): The AES secret key
+
+    Returns:
+        msg (bytes): The decrypted version of the ciphertext input
+    """
     aes = AES.new(key, AES.MODE_ECB)
     msg = aes.decrypt(ciphertext)
     return msg
 
 if __name__ == '__main__':
+    """
+    Tests both of the above AES ECB mode decryption functions on the encrypted
+    text file with known key.
+    """
     with open('set1_challenge7.txt', 'r') as f:
         ciphertext = b64decode(f.read())
         key = b'YELLOW SUBMARINE'
-        msg = decrypt_aes_ecb2(ciphertext, key)
+        msg = decrypt_aes_ecb1(ciphertext, key)
         print(msg.decode())
+        assert(decrypt_aes_ecb1(ciphertext, key)==decrypt_aes_ecb2(ciphertext,key))
--- set1_challenge8.py
+++ set1_challenge8.py
@@ -1,52 +1,23 @@
-from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
-from cryptography.hazmat.backends import default_backend
-from Crypto import cipher
-from base64 import b64encode, b64decode
 from binascii import hexlify, unhexlify
 
-def decrypt_aes_ecb(ciphertext, key):
-    backend = default_backend()
-    cipher = Cipher(algorithms.AES(key), modes.ECB(), backend=backend)
-    decryptor = cipher.decryptor()
-    msg = decryptor.update(ciphertext) + decryptor.finalize()
-    return msg
-
 if __name__ == '__main__':
+    """
+    Reads through all lines in the input text file to find the one encrypted with
+    AES ECB mode. This can be detected by a duplicate block occurring when the
+    same 16 byte plaintext string is encrypted twice.
+    """
     with open('set1_challenge8.txt') as f:
         lines = f.readlines()
-        likely_list = list('etaoin shrdlcumwfgypbvkjxqz')
-        likely_list.reverse()
-        output = []
-        best_total = 0
-        for line in lines:
-            print('line: %s' % line.strip())
+        for i, line in enumerate(lines):
+            #print('line: %s' % line.strip())
             line = unhexlify(line.strip())
-            print('decoded line: %s' % line)
+            #print('decoded line: %s' % line)
             if len(line) % 16:
                 print('Error: line not integer number of blocks')
             else:
                 num_blocks = len(line) // 16
                 blocks = [line[i*16:(i+1)*16] for i in range(num_blocks)]
                 if len(set(blocks)) != num_blocks: # There is a duplicate
-                    output.append(line)
-            # for i in range(16):
-            #     block = line[i:i+16]
-            #     print(block)
-            #     print('len: %d' % len(block))
-            #     print('i: %d, index: %d' % (i, line.find(block)))
-            #     if line.find(block) > i:
-            #         key = bytes(block)
-            # #key = b'YELLOW SUBMARINE'
-            #         msg = decrypt_aes_ecb(line, key).decode()
-            #         print(msg)
-            #         total = 0
-            #         for c in msg:
-            #             if c in likely_list:
-            #                 total += likely_list.index(c)
-            #         if total >= best_total:
-            #             best_total = total
-            #             best_ctext = line
-            #             best_msg = msg
-        # print('Best ciphertext: %s' % best_ctext)
-        # print('Best msg: %s' % best_msg)
-        print('output: %s' % str(output))
+                    ans_line = line
+                    ans_num = i
+        print(f'ans_num: {ans_num}, ans_line: {ans_line}')
